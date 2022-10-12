2ab64a2534e5a00ed16d4a38986f09943c13c146
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-10-12 12:52:26 +0530

    Add files via upload

---
+++ controllers/robo_controller/robo_controller.py
@@ -0,0 +1,28 @@
+from controller import Robot, Keyboard, Motion
+
+timestep=32
+
+robot= Robot()
+keyboard = Keyboard()
+keyboard.enable(timestep)
+
+forward = Motion('../../motions/wave.motion')
+
+
+def startMotion(motion):
+    motion.play()
+   
+def printMessages():
+    print('press up to wave')
+     
+key = -1
+
+printMessages()
+
+while robot.step(timestep) != -1:
+
+    key = keyboard.getKey()  
+    
+    if key == Keyboard.UP:
+        startMotion(forward)
+   
